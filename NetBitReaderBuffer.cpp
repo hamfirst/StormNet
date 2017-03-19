ff200c8dbaedd615f5e9b531e1f7e7eafb179e84
Nick Weihs <nick.weihs@gmail.com>
2017-03-19 16:04:06 -0700

    Add optimization and maybe fix a problem on linux

--- NetBitReaderBuffer.cpp
+++ NetBitReaderBuffer.cpp
@@ -3,6 +3,7 @@
 #include "NetException.h"
 
 #include <string.h>
+#include <assert.h>
 
 NetBitReaderBuffer::NetBitReaderBuffer(void * buffer, std::size_t num_bytes)
   : m_Buffer((uint8_t *)buffer), m_Offset(0), m_NumBytes(num_bytes), m_Bit(0)
@@ -15,6 +16,20 @@ uint64_t NetBitReaderBuffer::ReadUBits(int num_bits)
   if (num_bits <= 0)
   {
     return 0;
+  }
+
+  if (num_bits == 64 && m_NumBytes > 8)
+  {
+    uint64_t * ptr = (uint64_t *)m_Buffer;
+    uint64_t val = *ptr;
+
+    val >>= m_Bit;
+    val |= ((uint64_t)(*(m_Buffer + 8) & ((1 << m_Bit) - 1))) << (64 - m_Bit);
+
+    m_NumBytes -= 8;
+    m_Buffer += 8;
+    m_Offset += 8;
+    return val;
   }
 
   if (m_NumBytes <= 0)
@@ -34,7 +49,8 @@ uint64_t NetBitReaderBuffer::ReadUBits(int num_bits)
     m_NumBytes--;
     m_Buffer++;
     m_Offset++;
-    return (val & mask_bits) | (ReadUBits(num_bits - read_bits) << read_bits);
+    uint64_t final_val = (val & mask_bits) | (ReadUBits(num_bits - read_bits) << read_bits);
+    return final_val;
   }
 
   m_Bit += num_bits;
