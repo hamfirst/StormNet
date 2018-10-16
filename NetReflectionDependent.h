612d9a4cdf9317286b09a87ee3a79baa3cede754
Nick Weihs <nick.weihs@gmail.com>
2018-10-16 00:50:58 -0700

    Got dependent serializers working

--- NetReflectionDependent.h
+++ NetReflectionDependent.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include "NetReflectionCommon.h"
+#include "NetException.h"
 
 template <typename ValueType, typename ParentType, typename Serializer>
 class NetReflectionDependent
@@ -30,17 +31,51 @@ public:
     return &m_Value;
   }
 
-  ValueType * operator * ()
+  ValueType & operator * ()
   {
-    return &m_Value;
+    return m_Value;
   }
 
-  const ValueType * operator * () const
+  const ValueType & operator * () const
   {
-    return &m_Value;
+    return m_Value;
   }
 
 private:
   ValueType m_Value;
 };
+
+
+template <typename ValueType, typename ParentType, typename Serializer, typename NetBitWriter>
+struct NetSerializer<NetReflectionDependent<ValueType, ParentType, Serializer>, NetBitWriter>
+{
+  void operator()(const NetReflectionDependent<ValueType, ParentType, Serializer> & val, NetBitWriter & writer)
+  {
+    auto parent = writer.GetParentObjectAs<ParentType>();
+    if (parent == nullptr)
+    {
+      NET_THROW(std::runtime_error("Could not properly cast parent to depenedent serializer type"));
+      return;
+    }
+
+    Serializer::Serialize(val, *parent, writer);
+  }
+};
+
+template <typename ValueType, typename ParentType, typename Serializer, typename NetBitReader>
+struct NetDeserializer<NetReflectionDependent<ValueType, ParentType, Serializer>, NetBitReader>
+{
+  void operator()(NetReflectionDependent<ValueType, ParentType, Serializer> & val, NetBitReader & reader)
+  {
+    auto parent = writer.GetParentObjectAs<ParentType>();
+    if (parent == nullptr)
+    {
+      NET_THROW(std::runtime_error("Could not properly cast parent to depenedent serializer type"));
+      return;
+    }
+
+    Serializer::Deserialize(val, *parent, reader);
+  }
+};
+
 
